61911e02bbc86084518dea7b7a933198fefdf9aa
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2021-03-13 20:42:32 -0800

    Crated custom model field: OrderField

---
+++ env/educa/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/saied/Documents/dev/Portfolio/4-elearning-platform/env/educa/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
